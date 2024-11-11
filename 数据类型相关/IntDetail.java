5f28f9f004c2c6683b5b6164665d75d0d3ad85ed
hansonpang <52949883+hansonpang@users.noreply.github.com>
2024-11-11 10:11:52 +0800

    Delete 数据类型相关 directory
    1

--- 数据类型相关/IntDetail.java
+++
@@ -1,8 +0,0 @@
-//≤‚ ‘
-public class IntDetail{
-	public static void main(String[] args){
-		int n1 = 10;
-		int n2 = 10L;
-		System.out.println();
-	}
-}
--- 数据类型相关/plus.java
+++
@@ -1,9 +0,0 @@
-//测试加法
-public class plus{
-	public static void main(String[] args){
-		System.out.println(100 + 98);
-		System.out.println("100" + 98);
-		System.out.println(100 + 3 + "hello");
-		System.out.println("hello" + 100 +3);
-	}
-}
